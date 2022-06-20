36ad5f4ec6fe80980194ab06a8d54601f9e79c83
Carlos Henrique <carlos.c.r.w@gmail.com>
2022-06-20 13:58:51 -0300

    fix: configuração rota e controller pagina principal

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const indexRoute = require('./src/routes/IndexRoute')
 
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/src/views')
 app.use(express.json());
+app.use('/',indexRoute)
 
 
 // Roda o express na porta definida
