47eef3ac220d58b3efd053958113c589dcab9978
Allison Vale <allison@DESKTOP-4A373I5.localdomain>
2022-07-06 13:05:57 -0300

    fix: corrigindo erro na rota

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const router = express.Router()
-const userController = require('../controllers/userController')
-
-router.get('/', userController.index);
-router.get('/create', userController.create);
-
-
-=======
 const express = require('express');
 const router = express.Router()
 const userController = require('../controllers/UserController')
@@ -16,5 +6,4 @@ router.get('/', userController.index);
 router.get('/create', userController.create);
 
 
->>>>>>> ca5a81fa3c38c60e2015352db2ec0db23c3d823f
 module.exports = router;
