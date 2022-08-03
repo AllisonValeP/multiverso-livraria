1459dbc634c432064ad795e5ee9c44b42f0a046d
Carlos Henrique <carlos.c.r.w@gmail.com>
2022-08-03 11:51:56 -0300

    fet: configuraçães .env

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const session = require("express-session");
+const sessionConfig = require("./src/config/session")
+
 const indexRoute = require('./src/routes/IndexRoute')
 const userRoute = require('./src/routes/UserRoute')
 const productRoute = require('./src/routes/ProductRoute')
@@ -13,6 +16,7 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/src/views')
 app.use(express.json());
+app.use(session({ secret: sessionConfig.secret , resave: sessionConfig.resave, saveUninitialized: sessionConfig.saveUninitialized}));
 
 
 app.use('/',indexRoute)
