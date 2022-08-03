2b8ab87f9f443842884a6c1d120bd9adb6a845bb
Carlos Henrique <carlos.c.r.w@gmail.com>
2022-08-03 11:54:06 -0300

    fix: rota de botão edit

--- src/routes/AdmRoute.js
+++ src/routes/AdmRoute.js
@@ -3,9 +3,9 @@ const router = express.Router()
 const admController = require('../controllers/AdmController')
 
 router.get('/', admController.index);
-router.get('/admProduct', admController.productShow);
-router.get('/admUser', admController.userShow);
-router.get('/admOrder', admController.orderShow);
+router.get('/product', admController.productShow);
+router.get('/admuser', admController.userShow);
+router.get('/admorder', admController.orderShow);
 
 
 
