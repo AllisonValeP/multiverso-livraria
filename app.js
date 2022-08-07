fe59bf67f16a84f5335341b3575075a1041326c4
Carlos Henrique <carlos.c.r.w@gmail.com>
2022-08-07 09:53:27 -0300

    fix: instação de novas dependencias e configuração

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const session = require("express-session");
-const sessionConfig = require("./src/config/session")
+const session = require('express-session');
+const sessionConfig = require('./src/config/session');
+const methodOverride = require('methid-override');
+const cookieParser = require('cookie-parser');
 
-const indexRoute = require('./src/routes/IndexRoute')
-const userRoute = require('./src/routes/UserRoute')
-const productRoute = require('./src/routes/ProductRoute')
-const userPainelRoute = require('./src/routes/UserPainelRoute')
-const admRoute = require('./src/routes/AdmRoute')
-const purchaseRoute = require('./src/routes/PurchaseRoute')
+const indexRoute = require('./src/routes/IndexRoute');
+const userRoute = require('./src/routes/UserRoute');
+const userPainelRoute = require('./src/routes/UserPainelRoute');
+const admRoute = require('./src/routes/AdmRoute');
+const purchaseRoute = require('./src/routes/PurchaseRoute');
 
-
+app.use(methodOverride("_method"));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/src/views')
+app.set('views', __dirname + '/src/views');
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
 app.use(session({ secret: sessionConfig.secret , resave: sessionConfig.resave, saveUninitialized: sessionConfig.saveUninitialized}));
 
 
