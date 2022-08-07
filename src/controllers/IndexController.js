8e2bd8198de39c069b5551af0fec7004f2985c27
Carlos Henrique <carlos.c.r.w@gmail.com>
2022-08-07 14:12:05 -0300

    fix: validação campos cadastro

--- src/controllers/IndexController.js
+++ src/controllers/IndexController.js
@@ -1,3 +1,5 @@
+const {validationResult} = require('express-validator');
+
 const indexController ={
   // centralizar as rotas de login, create, 
     index: (req, res)=>{
@@ -6,6 +8,16 @@ const indexController ={
       create: (req, res) => {
         return res.render("login-create")
     },
+    store:(req, res) => {
+      const errors = validationResult(req);
+      console.log(errors)
+      if(!errors.isEmpty()) {
+        res.render('login-create', {
+          errors : errors.mapped(),
+          old: req.body,
+        });
+    }
+  },
     loginShow: (req, res) => {
       return res.render("login")
     },
--- src/routes/IndexRoute.js
+++ src/routes/IndexRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router()
 const indexController = require('../controllers/IndexController')
+const validate = require('../helpers/express-validator')
  // centralizar as rotas de login, create, 
 
 router.get('/', indexController.index);
 router.get('/create', indexController.create);
+router.post('/create', validate.validateRegistration, indexController.store);
 router.get('/login', indexController.loginShow);
 
 module.exports = router;
