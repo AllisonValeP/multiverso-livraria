8c2acadb22815c132665548037a81b989d851080
Carlos Henrique <carlos.c.r.w@gmail.com>
2022-08-09 17:12:10 -0300

    fix: mostra e ocutar senha form cadastro

--- public/javascript/password.js
+++ public/javascript/password.js
@@ -1,5 +1,15 @@
 function showPassword() {
     let tipo = document.getElementById("senha-id");
+  
+    if (tipo.type == "password") {
+        tipo.type = "text";
+    } else {
+        tipo.type = "password";
+    }
+}
+function showPasswordConfirm() {
+    let tipo = document.getElementById("senhaConfim-id");
+  
     if (tipo.type == "password") {
         tipo.type = "text";
     } else {
