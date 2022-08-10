e419a1c6f5f4cacb0648fa67c3a9a73cd205ab3d
Allison Vale <allison@DESKTOP-4A373I5.localdomain>
2022-08-10 19:54:53 -0300

    Fix: mundando a pagina de edição de usuarios (adm)

--- public/javascript/user-painel.js
+++ public/javascript/user-painel.js
@@ -14,3 +14,21 @@ radios.forEach((item, index) => {
         forms[index].classList.add('open-form')
     })
 })
+
+// Script da pagina ADM USER
+
+// pegando em formato de lista todos os #form-item02 
+const formsAdmUser = document.querySelectorAll('#form-item02')
+// pegando em formato de lista todos os tab 
+const radiosAdmUser = document.querySelectorAll('.aba')
+
+//iterando o click para todos
+radiosAdmUser.forEach((item, index) => {
+    // console.log("radio:",item)
+    item.addEventListener('click', () => {
+//      
+        formsAdmUser.forEach(item => item.classList.remove('abrir-formulario'))
+//    
+        formsAdmUser[index].classList.add('abrir-formulario')
+    })
+})
