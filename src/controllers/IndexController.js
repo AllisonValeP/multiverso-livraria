d54d83e1a8a1e52cdfbc8166a8cbb6d1335bced3
Carlos Henrique <carlos.c.r.w@gmail.com>
2022-08-25 14:11:58 -0300

    feat: tabela User e configuração de associação db

--- src/controllers/IndexController.js
+++ src/controllers/IndexController.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require("path");
+const { Sequelize, User } = require("../models");
 const bcrypt = require('../helpers/bcrypt');
 const { validationResult } = require('express-validator');
 
@@ -9,23 +10,23 @@ const indexController = {
   // centralizar as rotas de login, create, 
   index: (req, res) => {
     return res.render('index',
-    {
-      title: 'Multiverso Livraria',
-      user: req.cookies.user,
-     
-     
-    });
+      {
+        title: 'Multiverso Livraria',
+        user: req.cookies.user,
+
+
+      });
   },
   register: (req, res) => {
-    return res.render("login-create",{
+    return res.render("login-create", {
       title: 'Cadastre-se Agora | Multiverso Livraria',
       user: req.cookies.user,
 
     })
   },
-  create: (req, res) => {
+  create: async (req, res) => {
     const errors = validationResult(req);
-   
+
     if (!errors.isEmpty()) {
       res.render('login-create', {
         title: 'Realize seu Cadastro | Multiverso Livraria',
@@ -33,42 +34,28 @@ const indexController = {
         old: req.body,
       });
     } else {
-      const usersJson = fs.readFileSync(
-        path.join(__dirname, "..", "data", "users.json"),
-        "utf-8"
-      )
-      const users = JSON.parse(usersJson);
       const { nome, sobrenome, email, senha } = req.body;
-      const newId = users[users.length - 1].id + 1;
-      const newUser = {
-        id: newId,
-        nome,
-        sobrenome,
-        email,
-        senha: bcrypt.generateHash(senha),
-        admin: false,
-        criadoEm: new Date(),
-        modificadoEm: new Date(),
-      };
-      users.push(newUser);
-      fs.writeFileSync(
-        path.join(__dirname, "..", "data", "users.json"),
-        JSON.stringify(users)
-      );
-     res.redirect("login");
+      const newUser = await User.create({
+        name: nome,
+        last_name: sobrenome,
+        email: email,
+        password: bcrypt.generateHash(senha),
+      });
+
+      return res.redirect("login");
     }
   },
   login: (req, res) => {
     return res.render("login",
-    {
-      title: 'Faça Login | Multiverso Livraria',
-      user: req.cookies.user,
-    
-    });
+      {
+        title: 'Faça Login | Multiverso Livraria',
+        user: req.cookies.user,
+
+      });
   },
-  auth:(req, res)=>{
+  auth: (req, res) => {
     res.clearCookie("user");
-    
+
 
     const usersJson = fs.readFileSync(
       path.join(__dirname, "..", "data", "users.json"),
@@ -103,14 +90,14 @@ const indexController = {
       JSON.stringify(userAuth, ["id", "nome", "sobrenome", "apelido", "admin"])
     );
     req.session.email = userAuth.email;
-    res.cookie("user", user);    
-      if(user.admin){
-        res.redirect("/adm");
-      }
-      if(!user.admin){
-        res.redirect("/");
-      }
-    
+    res.cookie("user", user);
+    if (user.admin) {
+      res.redirect("/adm");
+    }
+    if (!user.admin) {
+      res.redirect("/");
+    }
+
   },
   // Processamento do deslogar
   logout: (req, res) => {
---
+++ src/models/User.js
@@ -0,0 +1,40 @@
+const User = (sequelize, DataTypes) => {
+    const user = sequelize.define(
+        'User', {
+            id: {
+                type:DataTypes.INTEGER.UNSIGNED,
+                autoIncrement: true,
+                primaryKey: true,
+              },
+            name: {
+                type: DataTypes.STRING(100),
+                allowNull: false,
+            },
+            last_name:  {
+                type: DataTypes.STRING(100),
+                allowNull: false,
+            },   
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },  
+            password:{
+                type: DataTypes.STRING,
+                allowNull: false,
+            }, 
+            is_admin: DataTypes.BOOLEAN,    
+         
+        }, 
+        {
+            tableName: "user",
+          
+        }
+    )
+    // Usuario.associate = (models => {
+    //     Usuario.hasMany(models.Endereco, { as: "usuario_endereco", foreignKey: 'usuario_id' })
+    // })
+
+    return user
+};
+
+module.exports = User
