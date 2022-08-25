abbee23b7a7d96195b6b6eefbd214d30e1a0861b
Carlos Henrique <carlos.c.r.w@gmail.com>
2022-08-25 16:33:40 -0300

    fix: validar pelo banco

--- src/helpers/express-validator.js
+++ src/helpers/express-validator.js
@@ -1,35 +1,29 @@
 const { check } = require("express-validator");
-const fs = require("fs");
-const path = require("path");
+const { Sequelize, User } = require("../models");
+
 
 const validateRegistration = [
     check('nome').notEmpty().withMessage('O campo nome é obrigatório! ').isLength({ min: 3 }).withMessage('O nome deve ter pelo menos 3 caracteres!'),
     check('sobrenome').notEmpty().withMessage('O campo sobrenome é obrigatório').isLength({ min: 3 }).withMessage('O sobrenome deve ter pelo menos 3 caracteres!'),
-    check('email').isEmail().withMessage('O e-mail precisa ser válido').custom(async email => {
-        const usersJson = fs.readFileSync(
-            path.join(__dirname, "..", "data", "users.json"),
-            "utf-8"
-        );
-        const users = JSON.parse(usersJson);
-        console.log
-        const procuraEmail = await users.find((user) => {
-            if (user.email === email) {
-                return true;
+    check('email').isEmail().withMessage('O e-mail precisa ser válido').custom(async emailBody => {
+
+        const procuraEmail = await User.findOne({
+            where: {
+                email: emailBody
             }
-
         })
-
-        if (!email) {
+        if (!emailBody) {
             return Promise.reject("E-mail é obrigatório");
         }
         if (procuraEmail) {
+          
             return Promise.reject("E-mail já cadastrado");
         }
         if (!procuraEmail) {
-            return email;
+            return emailBody;
         }
     }),
-    check('senha').isLength({ min: 8 }).withMessage('A senha deve ter no minimo 8 caracteres'),
+check('senha').isLength({ min: 8 }).withMessage('A senha deve ter no minimo 8 caracteres'),
     check('confirmSenha').isLength({ min: 8 }).withMessage(' ').custom((confirmSenha, { req }) => {
         const senha = req.body.senha;
         if (senha !== confirmSenha) {
@@ -44,4 +38,4 @@ const validateRegistration = [
 
 
 
-module.exports = {validateRegistration};
+module.exports = { validateRegistration };
