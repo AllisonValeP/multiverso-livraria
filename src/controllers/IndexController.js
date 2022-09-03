ff7407a95939ecac948aa70bf470c51af9f59f77
Carlos Henrique <carlos.c.r.w@gmail.com>
2022-09-03 15:38:46 -0300

    fix: login pelo db

--- src/controllers/IndexController.js
+++ src/controllers/IndexController.js
@@ -53,50 +53,71 @@ const indexController = {
 
       });
   },
-  auth: (req, res) => {
+  auth: async (req, res) => {
     res.clearCookie("user");
-
-
-    const usersJson = fs.readFileSync(
-      path.join(__dirname, "..", "data", "users.json"),
-      "utf-8"
-    );
-
-    const users = JSON.parse(usersJson);
-
     const { email, senha } = req.body;
-    const userAuth = users.find((user) => {
-      if (user.email === email) {
-        if (bcrypt.compareSync(senha, user.senha)) {
-          return true;
-        }
-        // O if de cima é a mesma coisa da linha abaixo
-        // return bcrypt.compareHash(senha, user.senha);
+    try {
+      const userAuth = await User.findOne({ where: { email: email } });
+      // const checkPassword = await bcrypt.compareSync(senha, userAuth.password)
+      
+      if (!userAuth || !bcrypt.compareSync(senha, userAuth.password)) {
+        return res.render("login", {
+          title: "Faça Login | Multiverso Livraria",
+          user: req.cookies.user,
+          old: req.body,
+          error: {
+            message: "Email ou senha inválido",
+          },
+        });
       }
-    });
-
-    if (!userAuth) {
-      return res.render("login", {
-        title: "Faça Login | Multiverso Livraria",
-        user: req.cookies.user,
-        old: req.body,
-        error: {
-          message: "Email ou senha inválido",
-        },
-      });
-    }
-    // Filtra as chaves que o objeto irá ter
-    const user = JSON.parse(
-      JSON.stringify(userAuth, ["id", "nome", "sobrenome", "apelido", "admin"])
-    );
-    req.session.email = userAuth.email;
-    res.cookie("user", user);
-    if (user.admin) {
+      req.session.email = userAuth.email; 
+      res.cookie("user", userAuth);
+      console.log(userAuth.email)
+      if (userAuth.is_admin) {
       res.redirect("/adm");
     }
-    if (!user.admin) {
+    if (!userAuth.is_admin) {
       res.redirect("/");
+    }   
+
+    } catch (error) {
+      console.log("Qual erro:" + error);
+
+
     }
+
+    // const userAuth = users.find((user) => {
+    //   if (user.email === email) {
+    //     if (bcrypt.compareSync(senha, user.senha)) {
+    //       return true;
+    //     }
+    //     // O if de cima é a mesma coisa da linha abaixo
+    //     // return bcrypt.compareHash(senha, user.senha);
+    //   }
+    // });
+
+    // if (!userAuth) {
+    //   return res.render("login", {
+    //     title: "Faça Login | Multiverso Livraria",
+    //     user: req.cookies.user,
+    //     old: req.body,
+    //     error: {
+    //       message: "Email ou senha inválido",
+    //     },
+    //   });
+    // }
+    // // Filtra as chaves que o objeto irá ter
+    // const user = JSON.parse(
+    //   JSON.stringify(userAuth, ["id", "nome", "sobrenome", "apelido", "admin"])
+    // );
+    // req.session.email = userAuth.email;
+    // res.cookie("user", user);
+    // if (user.admin) {
+    //   res.redirect("/adm");
+    // }
+    // if (!user.admin) {
+    //   res.redirect("/");
+    // }
 
   },
   // Processamento do deslogar
