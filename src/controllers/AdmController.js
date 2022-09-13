e9976383a17f72e3f03dbb660bff2d32674dd6db
Carlos Henrique <carlos.c.r.w@gmail.com>
2022-09-12 22:00:58 -0300

    fix: tela create Product

--- src/controllers/AdmController.js
+++ src/controllers/AdmController.js
@@ -1,42 +1,72 @@
+
+const sharp = require("sharp");
+const path = require("path");
+
+const { Product, Image } = require("../models");
+
+
 const admController = {
-    index: (req, res) => {
-        return res.render("adm-painel",
-        {
-          title: 'Administrador | Multiverso Livraria',
-          user: req.cookies.user,
-        })
-    },
-    productShow: (req, res) => {
-       return res.render("adm-product",
-       {
-         title: 'Administrador | Multiverso Livraria',
-         user: req.cookies.user,
-       })
-    },
-    userShow: (req, res) => {
-        return res.render("adm-user",
-        {
-          title: 'Administrador | Multiverso Livraria',
-          user: req.cookies.user,
-        })
-    },
-    orderShow: (req, res) => {
-        return res.render("adm-order",
-        {
-          title: 'Administrador| Multiverso Livraria',
-          user: req.cookies.user,
-        })
-    },
-    profileShow: (req, res) => {
-        return res.render("adm-profile",
-        {
-          title: 'Administrador | Multiverso Livraria',
-          user: req.cookies.user,
-        })
-    },
-    update: (req, res) => {},
-    delete: (req, res) => {},
-    destroy: (req, res) => {},
-  };
-  
-  module.exports = admController;
+  index: (req, res) => {
+    return res.render("adm-painel",
+      {
+        title: 'Administrador | Multiverso Livraria',
+        user: req.cookies.user,
+      })
+  },
+  productShow: (req, res) => {
+    return res.render("adm-product",
+      {
+        title: 'Administrador | Multiverso Livraria',
+        user: req.cookies.user,
+      })
+  },
+  createProduct: (req, res) => {
+    return res.render("adm-create-product",
+      {
+        title: 'Criar Produto | Multiverso Livraria',
+
+      })
+
+  },
+  updateProduct: async (req, res) => {
+    const { name, description, price, stock, publisher_id, author_id, category_id } = req.body
+    const file = req.file
+        console.log(file)
+    const teste = await sharp(file.path).metadata()
+    console.log(teste);
+    
+    try {
+    
+
+    } catch (err) {
+      console.log("Erro  do processar updateProduct");
+
+    }
+  },
+  userShow: (req, res) => {
+    return res.render("adm-user",
+      {
+        title: 'Administrador | Multiverso Livraria',
+        user: req.cookies.user,
+      })
+  },
+  orderShow: (req, res) => {
+    return res.render("adm-order",
+      {
+        title: 'Administrador| Multiverso Livraria',
+        user: req.cookies.user,
+      })
+  },
+  profileShow: (req, res) => {
+    return res.render("adm-profile",
+      {
+        title: 'Administrador | Multiverso Livraria',
+        user: req.cookies.user,
+      })
+  },
+  update: (req, res) => { },
+  delete: (req, res) => { },
+  destroy: (req, res) => { },
+};
+
+module.exports = admController;
--- src/routes/AdmRoute.js
+++ src/routes/AdmRoute.js
@@ -3,9 +3,15 @@ const router = express.Router()
 const admController = require('../controllers/AdmController')
 const isAuthAdm = require('../middlewares/authAdm');
 const indexController = require('../controllers/IndexController');
+const upload = require('../helpers/multer');
+const imgCompress = require('../middlewares/file-compressImg');
 
 router.get('/', isAuthAdm,admController.index);
 router.get('/product',isAuthAdm,admController.productShow);
+router.get('/create-product',isAuthAdm,admController.createProduct);
+router.post('/create-product',isAuthAdm,upload.single('file'),imgCompress,admController.updateProduct);
+
+
 router.get('/user', isAuthAdm,admController.userShow);
 router.get('/order', isAuthAdm,admController.orderShow);
 router.get('/profile', isAuthAdm,admController.profileShow);
