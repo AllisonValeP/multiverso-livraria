7020d56c2db90bfc6eb9f8273587d1bf59c3b150
Carlos Henrique <carlos.c.r.w@gmail.com>
2022-09-27 20:07:57 -0300

    fix: validação create product

--- src/helpers/validator-poduct-create.js
+++ src/helpers/validator-poduct-create.js
@@ -1,13 +1,14 @@
 const { check, body } = require("express-validator");
-const { Product, Publisher, Author, Category, Image, Address, User, Order } = require("../models");
+
 
 
 
 const validateProduct = [
     check('name').notEmpty().withMessage('Preencha o titulo! '),
     check('stock').notEmpty().withMessage('Preencha a quantidade! ').isDecimal().withMessage('Deve ser um numero. '),
-  
-    
+    check('price').notEmpty().withMessage('Preencha o valor! ').isDecimal().withMessage('Deve ser um numero. '),
+    check('description').notEmpty().withMessage('Preencha a descrição! ').isLength({ min: 50 }).withMessage('Mínimo 50 caracteres! '),
+    check('file').notEmpty().withMessage('Selecione uma imagens!')
 ]
 
 
